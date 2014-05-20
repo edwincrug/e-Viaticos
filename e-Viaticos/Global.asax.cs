a0c8bb830e49d3e32f62c78fb07a8fd647fd89f7
edwincrug <edwincrug@gmail.com>
2014-05-20 15:18:41 -0500

    Global ASAX

--- e-Viaticos/Global.asax.cs
+++ e-Viaticos/Global.asax.cs
@@ -17,5 +17,18 @@ namespace e_Viaticos
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            //---->Please set chatty values into session variables<------
+            //Required Fields
+
+            Session["UserLogin"] = "";      //cookies.strUserID
+            Session["StepName"] = "";       //tasks.GetTaskXMLElement("Keywords.StepLabel").ToString
+            Session["UltimusNumber"] = "";  //Convert.ToInt32(tasks.GetTaskXMLElement("Keywords.IncidentNumber
+            Session["ProcessSummary"] = ""; //tasks.GetTaskXMLElement("Keywords.IncidentSummary").ToString();
+
+        }
+
     }
 }
